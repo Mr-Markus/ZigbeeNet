a5a1e5880e2d380da004b97fc8ef08332b972c8e
Markus Lideck <markus.lideck@outlook.de>
2018-10-03 09:27:40 +0200

    ZCL added

---
+++ src/ZigbeeNet.ZCL/ZclPacket.cs
@@ -0,0 +1,26 @@
+﻿using BinarySerialization;
+using System;
+
+namespace ZigbeeNet.ZCL
+{
+    public class Frame
+    {
+        public Frame(FrameHeader header)
+        {
+            Header = header;
+        }
+
+        public Frame(FrameHeader header, byte[] payload)
+        {
+            Header = header;
+            Payload = payload;
+        }
+
+        [FieldOrder(0)]
+        [FieldLength(5)]
+        public FrameHeader Header { get; set; }
+
+        [FieldOrder(1)]
+        public byte[] Payload { get; set; }
+    }
+}
