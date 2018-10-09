69b3d3cc3cac5dfef3186aa21b424766b4fda7e1
Markus Lideck <markus.lideck@outlook.de>
2018-10-09 23:37:21 +0200

    ZpiObject Extension added

---
+++ src/ZigbeeNet.CCZnp/ZpiObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZigbeeNet.CC
+{
+    public static class ZpiObjectExtensions
+    {
+        public static T ToSpecificObject<T>(this ZpiObject zpiObject) where T : ZpiObject
+        {
+            return (T)Activator.CreateInstance(typeof(T), zpiObject);
+        }
+    }
+}
