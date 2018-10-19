a516415b73716191e91f516d9722ecd03c6dca3a
Markus Lideck <markus.lideck@outlook.de>
2018-10-19 22:42:49 +0200

    Missing file added

---
+++ ZigbeeNet/CC/Layers/DEBUG/Enums/DebugCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZigbeeNet.CC.DEBUG
+{
+    public enum DebugCommand {
+        bdbStartCommissioning = 5
+    }
+}
