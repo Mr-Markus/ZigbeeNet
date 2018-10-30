f56c9a9bb054e4b4776815311c67dc37bcb49103
Markus Lideck <markus.lideck@outlook.de>
2018-10-30 17:52:01 +0100

    Comments added

--- ZigbeeNet/CC/Packet/ZDO/ZDO_SIMPLE_DESC_RSP.cs
+++ ZigbeeNet/CC/Packet/ZDO/ZDO_SIMPLE_DESC_RSP.cs
@@ -4,32 +4,71 @@ using System.Text;
 
 namespace ZigbeeNet.CC.Packet.ZDO
 {
+    /// <summary>
+    /// This callback message is in response to the ZDO Simple Descriptor Request 
+    /// </summary>
     public class ZDO_SIMPLE_DESC_RSP : AsynchronousRequest
     {
         private const int MIN_DESC_LEN = 8;
 
+        /// <summary>
+        /// Specifies the message’s source network address
+        /// </summary>
         public ZAddress16 SrcAddr { get; private set; }
 
+        /// <summary>
+        /// This field indicates either SUCCESS or FAILURE
+        /// </summary>
         public PacketStatus Status { get; private set; }
 
+        /// <summary>
+        /// Specifies Device’s short address that this response describes
+        /// </summary>
         public ZAddress16 NwkAddr { get; private set; }
 
+        /// <summary>
+        /// Specifies the length of the simple descriptor 
+        /// </summary>
         public byte Len { get; private set; }
 
+        /// <summary>
+        /// Specifies Endpoint of the device 
+        /// </summary>
         public byte Endpoint { get; private set; }
 
+        /// <summary>
+        /// The profile Id for this endpoint
+        /// </summary>
         public DoubleByte ProfileId { get; private set; }
 
+        /// <summary>
+        /// The Device Description Id for this endpoint
+        /// </summary>
         public DoubleByte DeviceId { get; private set; }
 
+        /// <summary>
+        /// Defined as the following format 0 – Version 1.00 0x01-0x0F – Reserve
+        /// </summary>
         public byte DeviceVersion { get; private set; }
 
+        /// <summary>
+        /// The number of input clusters in the InClusterList
+        /// </summary>
         public byte NumInClusters { get; private set; }
 
+        /// <summary>
+        /// List of input cluster Id’s supported
+        /// </summary>
         public DoubleByte[] InClusterList { get; private set; }
 
+        /// <summary>
+        /// The number of output clusters in the OutClusterLis
+        /// </summary>
         public byte NumOutClusters { get; private set; }
 
+        /// <summary>
+        /// List of output cluster Id’s supported
+        /// </summary>
         public DoubleByte[] OutClusterList { get; private set; }
 
         public ZDO_SIMPLE_DESC_RSP(byte[] framedata)
