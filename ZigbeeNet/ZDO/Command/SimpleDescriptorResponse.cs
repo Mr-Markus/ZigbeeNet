b5fd8072676838c19f96a0f04b7c5183a6b35bb3
WIRTZNI <WIRTZNI@PC4461>
2018-12-05 17:28:35 +0100

    renaming

--- ZigbeeNet/ZDO/Command/SimpleDescriptorResponse.cs
+++ ZigbeeNet/ZDO/Command/SimpleDescriptorResponse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ZigbeeNet.ZDO.Command
+namespace ZigBeeNet.ZDO.Command
 {
     /**
      * Simple Descriptor Response value object class.
